900c8ed677dd6c6431367918266f0d7f2ae272da
anshonweb <ansh.a.3112@gmail.com>
2024-08-24 01:24:55 +0530

    changes

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,6 +1,6 @@
 import discord 
 from discord.ext import commands
-
+import requests
 
 
 class Numbers(commands.Cog):
@@ -8,15 +8,35 @@ class Numbers(commands.Cog):
         self.bot = bot
     
     @commands.Cog.listener()
-    
-   
-
     async def on_ready(self):
         print('Moderation cog loaded')
         channel = self.bot.get_channel(1246534324925501521)
-        await channel.send('Moderation cog loaded.')   
+        await channel.send('Moderation cog loaded.')
+    
+    
+
+    @commands.hybrid_group(name="parent" ,  with_app_command=True, description="Different Moderation Commands")
+    @commands.guild_only()  # Ensures this command only works in guilds (servers)
+    async def parent_command(self, ctx: commands.Context) -> None:
+          """
+    We even have the use of parents. This will work as usual for ext.commands but will be un-invokable for app commands.
+    This is a discord limitation as groups are un-invokable.
+    """
+    ...   # nothing we want to do in here, I guess!
+    @parent_command.command(name='avatar')
+    async def avatar(self, ctx : commands.Context , member: discord.Member):
+        embed=discord.Embed(title= member._user)
+        embed.set_image(url='{}'.format(member.display_avatar))
+        await ctx.send(embed=embed)
+
+    @parent_command.command(name='numbers')    
+    async def hello(self,ctx: commands.Context, numbers: int):
+        response=requests.get(f'http://numbersapi.com/{numbers}')
+        embed=discord.Embed(description=response.text)
         
+        await ctx.send(embed=embed)
+    
 
-
+    
 async def setup(bot):
     await bot.add_cog(Numbers(bot))
--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import logging
 import logging.handlers
 
 
-intents=discord.Intents.default()
-intents.message_content=True
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 logging.getLogger('discord.http').setLevel(logging.INFO)
@@ -25,25 +23,24 @@ logger.addHandler(handler)
 
 load_dotenv('.env')
 
+intents=discord.Intents.default()
+intents.message_content=True
+
+
 class DiscordBot(commands.Bot):
     def __init__(self) -> None:
         super().__init__(
-            command_prefix='.',
+            command_prefix='$',
             intents=intents,
             help_command=None,
         )
 
     
-    async def on_ready(self):
+    async def on_ready(self): 
         print(f'Logged in as {self.user}')
         channel = bot.get_channel(1246534324925501521)
         await channel.send(f"Logged in as {self.user}")
-
-        
-    async def on_message(self, message):
-        print(f'message sent by {message.author} contains {message.content}')
-
-    
+   
     async def load_cogs(self) -> None:
         """
         The code in this function is executed whenever the bot will start.
@@ -52,19 +49,31 @@ class DiscordBot(commands.Bot):
            if filename.endswith('.py'):
              await bot.load_extension(f'cogs.{filename[:-3]}')
 
-                
+            
+    
+
+    
 
     async def setup_hook(self) -> None:
         """
         This will just be executed when the bot starts the first time.
         """
         await self.load_cogs()
+
         
         
-
-
-
-
 
 bot = DiscordBot()
+
+@bot.command()
+async def sync(ctx) -> None:
+        """
+        Syncs the bot's application commands (slash commands) with Discord.
+        This command is restricted to the user with the ID specified in the .env file.
+        """
+        if str(ctx.author.id) == os.getenv("USER_ID"):
+            fmt = await ctx.bot.tree.sync(guild=ctx.guild)
+            await ctx.send(f'Synced {len(fmt)} commands.')
+        else:
+            await ctx.send("You do not have permission to sync commands.")
 bot.run(os.getenv("TOKEN") ,  log_handler=None)
